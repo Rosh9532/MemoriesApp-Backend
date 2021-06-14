f4dffefed086d343039b71043016ba2c3dec6026
Rosh9532 <roshnichauhan4800@gmail.com>
2021-06-15 01:08:27 +0530

    removed unwanted code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,27 +10,6 @@ const router=express.Router();
 
 //auth with google
 router.get('/google',passport.authenticate('google',{scope:['profile']}))
-// })
-
-// router.get('/goo',(req,res)=>{
-//     axios.get('http://localhost:2000/auth/google',passport.authenticate('google',{scope:['profile']}))
-//     .then(()=>{
-//         axios.get('http://localhost:2000/auth/google/callback',passport.authenticate('google',{
-//             session:false
-//         }),(req,res)=>{
-//             console.log(req.user)
-//             const token=jwt.sign({_id:req.user._id,},process.env.SECRET_KEY,{expiresIn:'1d'});
-//             const displayName=req.user.displayName
-//             console.log(token)
-//             console.log(displayName)
-            
-//         })
-//     })
-//     return res.status(200).json({
-//         token,
-//         displayName
-//     })
-// })
 
 router.get('/google/callback',passport.authenticate('google',{
     session:false
